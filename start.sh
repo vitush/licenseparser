4b84ab97aa26ff449a8d5ad57f8f355ef0f4ee3b
Andriy Vitushynskyy <avitushynskyy@gmail.com>
2013-12-10 13:18:00 +0200

    Added start-stop script

--- start.sh
+++ start.sh
@@ -1,3 +1,33 @@
-#!/bin/bash
+#! /bin/sh
 
-python ./manage.py runserver
+### BEGIN INIT INFO
+# Provides:          licenses
+# Required-Start:    $local_fs $remote_fs
+# Required-Stop:
+# X-Start-Before:
+# Default-Start:     3 4 5
+# Default-Stop:
+# Short-Description: Licenses Management tool
+# Description: Licenses Management tool.
+### END INIT INFO
+
+CMD='python ./manage.py runserver 0.0.0.0:8000'
+
+set -e
+
+case "$1" in
+  start)
+        $CMD
+        ;;
+  stop)
+        kill ` ps ax | grep runserver  | grep -v grep | awk '{ print $1}' `
+
+        ;;
+  reload|restart|force-reload|status)
+        ;;
+  *)
+        echo "Usage: $CMD {start|stop|restart|force-reload|status}" >&2
+        exit 1
+        ;;
+esac
+
